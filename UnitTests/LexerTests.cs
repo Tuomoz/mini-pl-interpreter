e589189b68b4fc438a446bfa72c99517f1633fcf
Tuomo Kärkkäinen <tuomon.email@gmail.com>
2016-02-14 19:33:05 +0200

    Add unit tests project

---
+++ UnitTests/LexerTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Interpreter;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class LexerTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            SourceReader source = new SourceReader(new System.IO.StringReader("123 456 moi"));
+            Lexer lexer = new Lexer(source);
+            Assert.AreEqual(lexer.getNextToken().ToString(), "Number: 123");
+        }
+    }
+}
