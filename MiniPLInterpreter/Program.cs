c6115bd1891ed324ca6c4b04dd7d946465042aa4
Tuomo Kärkkäinen <tuomon.email@gmail.com>
2016-04-03 20:52:06 +0300

    Main program

--- MiniPLInterpreter/Program.cs
+++ MiniPLInterpreter/Program.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using Lexer;
+using Frontend;
+using System.IO;
 
 namespace Interpreter
 {
@@ -9,10 +10,56 @@ namespace Interpreter
     {
         static void Main(string[] args)
         {
-            SourceReader source = new SourceReader(new System.IO.StringReader("var test 123"));
-            Scanner lexer = new Scanner(source);
-            foreach (Token token in lexer.GetTokens())
-                System.Console.WriteLine(token);
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Usage: interpreter filename [-p]\n\n-p: Print the parsed abstract syntax tree.");
+                return;
+            }
+            string filename = args[0];
+            StreamReader sourceFile;
+            try
+            {
+                sourceFile = new StreamReader(filename);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("File " + filename + " not found.");
+                return;
+            }
+            
+            SourceReader source = new SourceReader(sourceFile);
+            ErrorHandler errors = new ErrorHandler();
+            Scanner lexer = new Scanner(source, errors);
+            Parser parser = new Parser(lexer, errors);
+            StmtList ast = parser.Parse();
+            TypeCheckerVisitor checker = new TypeCheckerVisitor(errors, ast);
+            SymbolTable SymbolTable = checker.TypeCheck();
+            if (errors.HasErrors)
+            {
+                Console.WriteLine("Given program contains following errors:");
+                foreach (Error error in errors.GetErrors())
+                {
+                    Console.WriteLine(error);
+                }
+                return;
+            }
+            if (args.Length > 1 && args[1] == "-p")
+            {
+                Console.WriteLine("Parsed abstact syntax tree:");
+                AstPrinterVisitor printer = new AstPrinterVisitor(ast);
+                printer.Print();
+                Console.WriteLine();
+                Console.WriteLine("Program output:");
+            }
+            ExecutorVisitor executor = new ExecutorVisitor(SymbolTable, ast);
+            try
+            {
+                executor.Execute();
+            }
+            catch (RuntimeException e)
+            {
+                Console.WriteLine("Runtime exception: " + e.Message);
+            }
         }
     }
 }
